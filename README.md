ed6fbc1fd375ef27ff8982ecafc63c53ede1f4b5
ramez1212 <317170274@sha.edu.eg>
2022-12-04 04:12:15 +0200

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# puresoft_tasks
+
+A new Flutter project.
+
+## Task 1 (Responsiveness and adaptability)  
+
+- create flutter app .
---
+++ android/app/src/main/kotlin/com/example/puresoft_tasks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.puresoft_tasks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
